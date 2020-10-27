037b4c4abc792ea79f6cb4db18b43d0d076d448e
Impulse <smetiyk@gmail.com>
2020-10-27 21:48:46 +0200

    test website

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,10 +4,11 @@ import {
   HeaderMenuItemText,
   HeaderMenuItem,
   Logo,
-  MainWrapper
+  MainWrapper,
 } from "./header.styled";
 import { headerItems } from "./header-data";
 import logo from "../../img/logo-wtt-w.svg";
+import search from "../../img/search.png";
 
 export const Header = () => {
   return (
@@ -15,16 +16,24 @@ export const Header = () => {
       <Block>
         <HeaderMenuItem>
           <Logo src={logo} />
-          <HeaderMenuItemText>Will you try?</HeaderMenuItemText>
+          <span className="Header__will-you">Will you try?</span>
         </HeaderMenuItem>
         <HeaderMenuItem>
           {headerItems.map((el, id) => (
             <HeaderMenuItemText key={id}>{el}</HeaderMenuItemText>
           ))}
         </HeaderMenuItem>
-        <HeaderMenuItem>
-          <HeaderMenuItemText>ws search</HeaderMenuItemText>
-        </HeaderMenuItem>
+        <div className="Header__search-link">
+          <a href="https://google.com">ws</a>
+          <div>
+            <a href="*">
+              <i></i>
+            </a>
+          </div>
+        </div>
+        <div className="Header__software">
+          SOFTWARE AND<br/> DEVELOPMENT<br/> AGENCY
+        </div>
       </Block>
     </MainWrapper>
   );
--- src/components/header/header.styled.jsx
+++ src/components/header/header.styled.jsx
@@ -4,17 +4,29 @@ const green = "#29B82E";
 
 const MainWrapper = styled.div`
   height: 85vh;
-  background: #A9F5F2 url('./img1.jpg');
+  background: #96abaa;
   padding-top: 25px;
 `;
 
 const HeaderMenuItem = styled.div`
   height: 100%;
   display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  > img {
+    width: 30px;
+    height: 30px;
+  }
+  > .Header__will-you {
+    font-size: 20px;
+    font-weight: 800;
+    color: white;
+  }
 `;
 
 const HeaderMenuItemText = styled.span`
-  padding: 10px 20px;
+  padding: 0 20px;
   height: 100%;
   text-align: center;
   display: flex;
@@ -48,6 +60,46 @@ const Block = styled.div`
   height: 60px;
   flex-direction: row;
   justify-content: space-around;
+  color: white;
+  position: relative;
+  .Header__software{
+    position: absolute;
+    top: 18rem;
+    left: 25rem;
+    font-size: 40px;
+    font-weight: 600;
+    width: max-content;
+    letter-spacing: 5px;
+  }
+  .Header__search-link {
+    display: flex;
+    align-items: center;
+    > div {
+      > a {
+        text-decoration: none;
+        :hover {
+          transition: 0.2s;
+          color: white;
+        }
+        > i {
+          ::before {
+            content: "⚲";
+          }
+        }
+      }
+    }
+    > a {
+      text-decoration: none;
+      padding: 0 5px;
+      font-size: 20px;
+      font-weight: 800;
+      :hover {
+        transition: 1s;
+        color: white;
+        box-shadow: 0px 0px 3px 2px;
+      }
+    }
+  }
 `;
 
 export { HeaderMenuItem, HeaderMenuItemText, Logo, Block, MainWrapper };
