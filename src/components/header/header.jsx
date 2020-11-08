c969c97b8daa2cb2f2e62e5e328e6bf38802eb34
Impulse <smetiyk@gmail.com>
2020-11-08 17:41:00 +0200

    styles a

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import './header.scss'
+import logo from '../../img/logo-wtt-w.svg'
+import { Button } from 'antd'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <div className="Header__wrapper">
       <div className="Header__block">
-        <span>icon</span>
-        <span>Try to be Happy</span>
+        <Link to="/">
+          <img src={logo} alt="logo" />
+          <span>Try to be Happy</span>
+        </Link>
       </div>
-      <div className="Header__block">
+      <div className="Header__links">
         <a href="/#">About</a>
         <a href="/#">Expertise</a>
         <a href="/#">Work</a>
-        <a href="/#">Contact</a>
+        <a href="/contact">Contact</a>
       </div>
-      <div className="Header__block">
-        <a href="/#">Search</a>
-        <a href="/#">Search</a>
+      <div className="Header__search">
+        <a href="/#">Search</a> <a href="/#">Search</a>
       </div>
     </div>
   )
